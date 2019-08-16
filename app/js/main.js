20b73d14c6d73457e0267b7c248789ce02c4a22b
apalion1493 <39518411+apalion1493@users.noreply.github.com>
2019-08-16 11:14:46 +0300

    commit

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,13 @@
 var btn = $('.navigation-block__btn');
+
+$(window).scroll(function() {
+    if ($(window).scrollTop() > 300) {
+        btn.addClass('show');
+    } else {
+        btn.removeClass('show');
+    }
+});
+
 btn.on('click', function(e) {
     e.preventDefault();
     $('html, body').animate({scrollTop:0}, '300');
